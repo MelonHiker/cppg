6b846603baa0b74c6816c350403598ec78872270
MelonHiker <samjack123441@gmail.com>
2024-12-29 21:11:05 +0800

    feat: add tqdm

--- src/cppg.py
+++ src/cppg.py
@@ -1,9 +1,11 @@
-from configs.config_loader import settings
-from problem_generator import generate_problem
-from problem_validator import validate_problem
-from problem_reflection import reflect_problem
-from reflection_validator import validate_reflection
-from log import clear_log
+from src.configs.config_loader import settings
+from src.stage.problem_generator import generate_problem
+from src.stage.problem_validator import validate_problem
+from src.stage.problem_reflection import reflect_problem
+from src.stage.reflection_validator import validate_reflection
+from src.log import clear_log
+from tqdm import tqdm
+import json
 import os
 
 class CPPG:
@@ -14,13 +16,26 @@ class CPPG:
         self.story = story
         os.environ["GEMINI_API_KEY"] = settings.api_key
 
-    def generate(self):
-        problem = generate_problem(self.skill_1, self.skill_2, self.story)
-        problem = validate_problem(problem, self.skill_1, self.skill_2)
-        problem = reflect_problem(problem)
-        # problem = validate_reflection()
-        return problem
+    def generate(self, file_path) -> None:
+        with tqdm(total=100) as pbar:
+            pbar.set_description("Generating problem")
+            problem = generate_problem(self.skill_1, self.skill_2, self.story)
+            pbar.update(20)
+            
+            pbar.set_description("Validating problem")
+            problem = validate_problem(problem, self.skill_1, self.skill_2)
+            pbar.update(20)
 
-if __name__ == "__main__":
-    cppg = CPPG("math", "graphs")
-    print(cppg.generate())
+            pbar.set_description("Reflecting on problem")
+            reflection = reflect_problem(problem)
+            pbar.update(20)
+
+            pbar.set_description("Validating reflection")
+            problem = validate_reflection(problem, reflection, self.skill_1, self.skill_2)
+            pbar.update(20)
+
+            pbar.set_description("Save file")
+            with open(file_path, 'w') as f:
+                json.dump(problem, f)
+            pbar.update(20)
+    
