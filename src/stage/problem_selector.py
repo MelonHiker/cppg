deb8774d625b39fe6e3790284a1b345cde9eb812
MelonHiker <samjack123441@gmail.com>
2025-01-14 19:53:38 +0800

    feat: select best problem

---
+++ src/stage/problem_selector.py
@@ -0,0 +1,20 @@
+from src.configs.config_loader import settings
+from litellm import completion
+
+def select_problem(problems: list, min_difficulty: int, max_difficulty: int, skill_1: str, skill_2: str, logger) -> str:
+    problems = "\n\n".join(problems)
+    system_prompt = settings.problem_selector_prompt.system
+    user_prompt = settings.problem_selector_prompt.user.format(min_difficulty=min_difficulty, max_difficulty=max_difficulty, skill_1=skill_1, skill_2=skill_2, problems=problems)
+    
+    logger.info(user_prompt)
+
+    response = completion(
+    model=settings.model,
+    temperaturel=settings.problem_selector_prompt.temperature,
+    messages=[{"role": "system", "content": system_prompt},
+              {"role": "user", "content": user_prompt}]
+    )
+    content = response.choices[0].message.content
+    
+    logger.info(content)
+    return content
